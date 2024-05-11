ef7cafa77a59fd651a27c5c3680575cc58cb5a85
anthonytawk1 <161260033+anthonytawk1@users.noreply.github.com>
2024-05-11 13:45:43 +0300

    Update audit.service.js

--- src/services/audit.service.js
+++ src/services/audit.service.js
@@ -1,15 +1,3 @@
-// timestamp: date of the API call,
-//! o microservice: name of the microservice that the call is going to,
-//! o url: url of the request,
-//! o method: method of the request,
-//? o statusCode: the status code of the request for example 200 for success,
-//? o userId: The user ID of the user who is calling the API or doing the action,
-//! o userAgent: The agent of the request,
-//? o result: The result of the API,
-//? o success: true/false if the request is an error or a success,
-//! o headers: request headers,
-//! o params: request params,
-//! o body: request body
 const Audit = require("../models/audit.model");
 class AuditService {
   async saveAuditResponse(response, _id) {
